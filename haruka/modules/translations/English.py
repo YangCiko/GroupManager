663445104a395b37c6dce4ff8c4bd9a49d15550f
YangchiN <67500757+YangchiN@users.noreply.github.com>
2020-06-30 11:45:58 +0530

    Update English.py

--- haruka/modules/translations/English.py
+++ haruka/modules/translations/English.py
@@ -212,19 +212,9 @@ or feature requests you might have :)
 If you enjoying using me and/or would like to help me survive in the wild, hit /donate to help fund/upgrade my VPS!
 """,
 
-    "send-help": """Hey there! My name is *{}*.
-I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of the things I can help you with.
+    "send-help": """All the below features are only for [Telegram Link](https://t.me/Share_Your_TelegramLinks) Group Admins
 
-Main commands available:
- - /start: Cool command to check if the bot is alive or not
- - /help: PM's you this message.
- - /help <module name>: PM's you info about that module.
- - /donate: information about how to donate!
- - /lang: Change bot language
- - /settings:
-   - in PM: will send you your settings for all supported modules.
-   - in a group: will redirect you to pm, with all that chat's settings.
-   {}
+Made with ❤️ by @Telegram
    """,
 
     "send-group-settings": """Hi there! There are quite a few settings for *{}* - go ahead and pick what
